717bc8da98120ed0f46c3bd747bae9e1190a445a
bchaindeveloper <bcdev3@protonmail.com>
2023-05-10 14:17:19 -0400

    Fixed API Connection

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import Link from "next/link"
 
-const Form = () => {
+const Form = ({ type, Post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing products with the world, and let your imagination run wild with any AI-powered platform.
+      </p>
+      <form onSubmit={handleSubmit}
+      className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
+        <label>
+          <span className="font-satoshi font-semibold text-base text-grey-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={postMessage.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value})}
+            placeholder="write your prompt here.."
+            required
+            className="from_textarea"
+            >
+
+          </textarea>
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-grey-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={postMessage.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value})}
+            placeholder="write your prompt here.."
+            required
+            className="from_textarea"
+            >
+
+          </textarea>
+        </label>
+      </form>
+
+    </section>
   )
 }
 
